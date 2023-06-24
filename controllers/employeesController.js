883c2b15fe76ed97ffee173fbe68547614583177
Harsha Gunawardane <mrgunawardaneofficial@gmail.com>
2023-06-24 13:36:53 +0530

    Add employee routes and test it

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,62 +1,143 @@
 const data = {
-    employees: require('../models/employees.json'),
-    setEmployees: function (data) { this.employees = data }
-}
+  employees: require("../models/employees.json"),
+  setEmployees: function (data) {
+    this.employees = data;
+  },
+};
+// import ORM to handle Database
+const { PrismaClient } = require("@prisma/client");
+const { th } = require("date-fns/locale");
+const { json } = require("express");
+const prisma = new PrismaClient();
 
-const getAllEmployees = (req, res) => {
-    res.json(data.employees);
-}
+const getAllEmployees = async (req, res) => {
+  try {
+    const employees = await prisma.employees.findMany();
+    res.json(employees);
+  } catch (error) {
+    throw error;
+  }
+};
 
-const createNewEmployee = (req, res) => {
-    const newEmployee = {
-        id: data.employees?.length ? data.employees[data.employees.length - 1].id + 1 : 1,
-        firstname: req.body.firstname,
-        lastname: req.body.lastname
+const createNewEmployee = async (req, res) => {
+  const { first_name, last_name } = req.body;
+
+  if (!first_name || !last_name) {
+    return res
+      .status(400)
+      .json({ message: "First and last names are required." });
+  }
+
+  const newEmployee = await prisma.employees.create({
+    data: {
+      first_name: first_name,
+      last_name: last_name,
+    },
+  });
+
+  res.status(201).json(newEmployee);
+};
+
+const updateEmployee = async (req, res) => {
+  const {
+    find_first_name,
+    find_last_name,
+    update_first_name,
+    update_last_name,
+  } = req.body;
+
+  if (
+    !find_first_name ||
+    !find_last_name ||
+    !update_first_name ||
+    !update_last_name
+  ) {
+    return res.status(400).json({ message: "Some arguments are missing" });
+  }
+  try {
+    // find employee registered or not
+    const foundEmployee = await prisma.employees.findFirst({
+      where: {
+        first_name: find_first_name,
+        last_name: find_last_name,
+      },
+    });
+
+    if (!foundEmployee) {
+      return res.status(400).json({
+        message: `Employee ${find_first_name} ${find_last_name} not found`,
+      });
+    }
+    const updatedEmployee = await prisma.users.update({
+      where: {
+        first_name: find_first_name,
+        last_name: find_last_name,
+      },
+      data: {
+        first_name: update_first_name,
+        last_name: update_last_name,
+      },
+    });
+
+    res.json(updatedEmployee);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const deleteEmployee = async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  try {
+    // find user registered or not
+    const foundEmployee = await prisma.users.findFirst({
+      where: {
+        id: id,
+      },
+    });
+    if (!foundEmployee) {
+      return res
+        .status(400)
+        .json({ message: `Employee ${req.params.first_name} not found` });
     }
 
-    if (!newEmployee.firstname || !newEmployee.lastname) {
-        return res.status(400).json({ 'message': 'First and last names are required.' });
-    }
+    const deleteEmployee = await prisma.users.delete({
+      where: { id: id },
+    });
 
-    data.setEmployees([...data.employees, newEmployee]);
-    res.status(201).json(data.employees);
-}
+    res.json({
+      message: `Employee ${req.params.foundEmployee.first_name} deleted`,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
 
-const updateEmployee = (req, res) => {
-    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
-    if (!employee) {
-        return res.status(400).json({ "message": `Employee ID ${req.body.id} not found` });
-    }
-    if (req.body.firstname) employee.firstname = req.body.firstname;
-    if (req.body.lastname) employee.lastname = req.body.lastname;
-    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
-    const unsortedArray = [...filteredArray, employee];
-    data.setEmployees(unsortedArray.sort((a, b) => a.id > b.id ? 1 : a.id < b.id ? -1 : 0));
-    res.json(data.employees);
-}
+const getEmployee = async (req, res) => {
+  const id = parseInt(req.params.id);
 
-const deleteEmployee = (req, res) => {
-    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
-    if (!employee) {
-        return res.status(400).json({ "message": `Employee ID ${req.body.id} not found` });
+  try {
+    // find user registered or not
+    const foundEmployee = await prisma.users.findFirst({
+      where: {
+        id: id,
+      },
+    });
+    if (!foundEmployee) {
+      return res
+        .status(400)
+        .json({ message: `Employee ${req.params.first_name} not found` });
     }
-    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
-    data.setEmployees([...filteredArray]);
-    res.json(data.employees);
-}
-
-const getEmployee = (req, res) => {
-    const employee = data.employees.find(emp => emp.id === parseInt(req.params.id));
-    if (!employee) {
-        return res.status(400).json({ "message": `Employee ID ${req.params.id} not found` });
-    }
-    res.json(employee);
-}
+    res / json(foundEmployee);
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
-    getAllEmployees,
-    createNewEmployee,
-    updateEmployee,
-    deleteEmployee,
-    getEmployee
-}
+  getAllEmployees,
+  createNewEmployee,
+  updateEmployee,
+  deleteEmployee,
+  getEmployee,
+};
