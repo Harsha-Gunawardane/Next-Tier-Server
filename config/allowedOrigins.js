265eee308fafc8ef806d2ed365e4359c301a5107
Nipuna Rahal <2020cs019@stu.ucsc.cmb.ac.lk>
2023-07-12 08:29:20 +0530

    Fixed errors of schema.prisma

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,3 @@
-const whitelist = [
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3000'
-];
+const whitelist = ["http://127.0.0.1:5500", "http://localhost:3500", "http://localhost:3000", "http://localhost:3002"];
 
 module.exports = whitelist;
