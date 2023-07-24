53c8fc8e8734a8d6bfd726fe3989b85f451b2314
Harsha Gunawardane <mrgunawardaneofficial@gmail.com>
2023-07-24 09:25:54 +0530

    add new routes in server file

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const path = require('path');
-const cookieParser = require('cookie-parser');
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 
 const PORT = process.env.PORT || 3500;
 
 // import custom middleware
-const { logger } = require('./middleware/logEvents');
-const errorHandler = require('./middleware/errorHandler');
-const credentials = require('./middleware/credentials');
-const verifyJWT = require('./middleware/verifyJWT');
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+const credentials = require("./middleware/credentials");
+const verifyJWT = require("./middleware/verifyJWT");
 
 // import custom files
-const corsOptions = require('./config/corsOptions');
+const corsOptions = require("./config/corsOptions");
 
 // custom middleware logger
-// app.use(logger);
+app.use(logger);
 
 // check credentials before cors
 app.use(credentials);
@@ -28,7 +29,7 @@ app.use(cors(corsOptions));
 // built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
 
-// built-in middleware for json 
+// built-in middleware for json
 app.use(express.json());
 
 // middleware for cookies
@@ -37,40 +38,46 @@ app.use(cookieParser());
 // Middleware to parse JSON
 app.use(bodyParser.json());
 
+// Apply the fileUpload middleware
+app.use(fileUpload({ createParentPath: true }));
+app.use(fileUpload());
+
 //serve static files
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use("/", express.static(path.join(__dirname, "/public")));
 
 // routes
-app.use('/', require('./routes/root'));
-app.use('/register', require('./routes/register'));
-app.use('/auth', require('./routes/auth'));
-app.use('/refresh', require('./routes/refresh'));
-app.use('/logout', require('./routes/logout'));
-app.use('/send-otp', require('./routes/send-otp'));
-app.use('/verify-otp', require('./routes/verify-otp'));
-app.use('/forgot-password', require('./routes/reset-password'));
+app.use("/", require("./routes/root"));
+app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
+app.use("/send-otp", require("./routes/send-otp"));
+app.use("/verify-otp", require("./routes/verify-otp"));
+app.use("/forgot-password", require("./routes/reset-password"));
 
 // check authentication of user
 app.use(verifyJWT);
 
-app.use('/employees', require('./routes/api/employees'));
-app.use('/user', require('./routes/api/user'));
-app.use('/notes', require('./routes/api/notes'))
+app.use("/employees", require("./routes/api/employees"));
+app.use("/user", require("./routes/api/user"));
+app.use("/notes", require("./routes/api/notes"));
 app.use("/staff", require("./routes/api/staff"));
+app.use("/stu", require("./routes/api/student"));
+app.use("/parent", require("./routes/api/parent"));
 
-app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if (req.accepts('json')) {
-        res.json({ "error": "404 Not Found" });
-    } else {
-        res.type('txt').send("404 Not Found");
-    }
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-    console.log('listening on port ' + PORT);
-})
+  console.log("listening on port " + PORT);
+});
