ade3642c1cb94b11eb8c6d12a107477a6a71d720
Harsha Gunawardane <mrgunawardaneofficial@gmail.com>
2023-07-24 09:26:21 +0530

    modify and add some routes to user

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const userController = require('../../controllers/user');
 
-// verify roles
-// const ROLES_LIST = require("../../config/roleList");
-// const verifyRoles = require("../../middleware/verifyRoles");
+const fileUpload = require("express-fileupload");
+const filePayloadExists = require("../../middleware/fileUpload/filePayloadExists");
+const fileExtLimitter = require("../../middleware/fileUpload/fileExtLimitter");
+const fileSizeLimitter = require("../../middleware/fileUpload/fileSizeLimitter");
 
-router.route("/:username").get(userController.getUser);
+const userProfileController = require("../../controllers/user/profilePicture");
+
+// middleware for file upload
+router.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
+
+router
+  .route("/profile-image")
+  .post(
+    // filePayloadExists(),
+    // fileExtLimitter([".jpg", ".png", ".jpeg"]),
+    // fileSizeLimitter(),
+    userProfileController.uploadProfilePicture
+  );
 
 module.exports = router;
