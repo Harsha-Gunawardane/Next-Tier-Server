af4c8ff01df868820b7a3ff1e39c7598046a054d
Harsha Gunawardane <mrgunawardaneofficial@gmail.com>
2023-07-24 09:28:55 +0530

    Modify verify jwt middleware

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -2,18 +2,20 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const verifyJWT = (req, res, next) => {
+  console.log('in verifyJWT')
   const authHeader = req.headers.authorization || req.headers.Authorization;
 
   // Check authorization
   if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
 
-  console.log(authHeader);
+  console.log('auth header');
   const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (err, decoded) => {
     if (err) return res.sendStatus(403);
 
     req.user = decoded.UserInfo.username;
     req.roles = decoded.UserInfo.roles;
+    
     next();
   });
 };
