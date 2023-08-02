b639e9573a5b97fffb69bf0770fcdcf75eeafca1
Harsha Gunawardane <mrgunawardaneofficial@gmail.com>
2023-08-02 14:27:47 +0530

    Fix a misstake in profile controller

--- controllers/user/profilePicture.js
+++ controllers/user/profilePicture.js
@@ -37,7 +37,7 @@ const uploadProfilePicture = async (req, res) => {
         },
       });
 
-      const imageUrl = `https://storage.googleapis.com/${process.env.GCS_FILEBUCKET}/${blob.name}`;
+      const imageUrl = `https://storage.googleapis.com/${process.env.GCS_BUCKET}/${blob.name}`;
 
       // insert new file to DB
       const newProfileData = {
